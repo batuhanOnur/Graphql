2be50e9ffde98e099208c1701d71129cf10ab8d2
batuhan <batuhanonur29@gmail.com>
2020-06-02 17:44:11 +0300

    graphql server installation

---
+++ movie-app/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const expressGraphql = require('express-graphql');
+
+const schema = require('./schema/schema');
+
+const app = express();
+
+// middleware 
+app.use('/graphql', expressGraphql({
+    schema
+}));
+
+app.listen(5000, () => {
+    console.log('server is running');
+})
